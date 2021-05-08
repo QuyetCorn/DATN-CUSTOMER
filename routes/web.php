205102acd4a1be73de3523d13c0f032d221bd6ee
Quyet Ngo <nguynhuan123@gmail.com>
2021-05-08 12:19:25 +0700

    New repo
    Làm lại repo khác

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Gr User
+Route::group(['namespace' => 'User'], function() {
+    Route::get('/trangchu', 'TrangChuController@index')->name('trangchu');
+    Route::get('/sanpham', 'SanPhamController@index')->name('sanpham');
+    Route::get('/tintuc', 'TinTucController@index')->name('tintuc');
+    Route::get('/chitietsanpham', 'CTSPController@index')->name('chitietsanpham');
+    Route::get('/chitiettintuc', 'TinTucController@chitiettintuc')->name('chitiettintuc');
+    Route::get('/dangnhapdangky', 'DangNhapDangKyController@index')->name('dangnhapdangky');
+    Route::get('/giohang', 'GioHangController@index')->name('giohang');
+    Route::get('/thanhtoan', 'ThanhToanController@index')->name('thanhtoan');
+    Route::get('/gioithieu', 'GioiThieuController@index')->name('gioithieu');
+    Route::get('/lienhe', 'LienHeController@index')->name('lienhe');
+
+    // Danh muc
+    Route::get('/balo', 'DanhMucController@baloIndex')->name('balo');
+    Route::get('/tuixach', 'DanhMucController@tuixachIndex')->name('balo');
+    Route::get('/vali', 'DanhMucController@valiIndex')->name('vali');
+    Route::get('/xemthem', 'DanhMucController@xemthemIndex')->name('xemthem');
+    Route::get('/sanphamkhuyenmai', 'DanhMucController@sanphamkhuyenmaiIndex')->name('sanphamkhuyenmai');
+    Route::get('/sanphammoi', 'DanhMucController@sanphammoiIndex')->name('sanphammoi');
+    Route::get('/sanphamnoibat', 'DanhMucController@sanphamnoibatIndex')->name('sanphamnoibat');
+});
