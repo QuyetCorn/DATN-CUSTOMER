f9e5f20b66c12cca5ddf72f3464f781fb9ecfe4e
Quyet Ngo <nguynhuan123@gmail.com>
2021-05-14 17:04:18 +0700

    Update

--- resources/views/user/layout/head.blade.php
+++ resources/views/user/layout/head.blade.php
@@ -26,4 +26,29 @@
         .lightboxContain img{float:none;max-width:100%;height:auto;max-height:100%;margin:0 auto}
     </style>
     <script src='assets/js/module.js'></script>
+    
+    <!-- Messenger Plugin chat Code -->
+    <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
+
+      <!-- Your Plugin chat code -->
+      <div class="fb-customerchat"
+        attribution="biz_inbox"
+        page_id="110554391206232">
+      </div>
 </head>
---
+++ resources/views/user/layout/popup_cart.blade.php
@@ -0,0 +1,43 @@
+<div id="popup-cart" class="modal fade" role="dialog">
+        <div id="popup-cart-desktop" class="clearfix">
+            <div class="title-popup-cart">
+                Bạn đã thêm <span class="cart-popup-name"></span> vào giỏ hàng
+            </div>
+            <div class="title-quantity-popup left_" onclick="window.location.href='/cart';">
+                Giỏ hàng của bạn <span class="hidden count_item_pr"></span>
+            </div>
+            
+            <div class="content-popup-cart">
+                <div class="thead-popup">
+                    <div style="width: 49.75%;" class="text-left">Sản phẩm</div>
+                    <div style="width: 15%;" class="text-center">Giá</div>
+                    <div style="width: 20%;" class="text-center">Số lượng</div>
+                    <div style="width: 15%;" class="text-right">Tổng tiền</div>
+                </div>
+                <div class="tbody-popup scrollbar-dynamic">
+                </div>
+                <div class="tfoot-popup">
+                    <div class="tfoot-popup-1 clearfix">
+                        <div class="title-quantity-popup popup-total right_ bottom_">
+                            <p class="tongtien">Tổng số thành tiền: <span class="total-price"></span></p>
+                        </div>
+                        <div class="pull-left popup-ship">
+                            <p class="hidden">Miễn phí giao hàng toàn quốc</p>
+                            <a class="hidden button btn-continue" title="Tiếp tục mua hàng" onclick="$('#popup-cart').modal('hide');"><span><span>Tiếp tục mua hàng</span></span></a>
+                        </div>
+                        <div class="pull-right popup-total">
+                            <p class="hidden">Phí vận chuyển: <span class="vanchuyen">Tính khi thanh toán</span></p>
+                            <p class="tongtien hidden">Tổng tiền: <span class="total-price"></span></p>
+                            <a class="button btn-proceed-checkout" title="Tiến hành đặt hàng" href="/checkout"><span>Tiến hành thanh toán</span></a>
+                        </div>
+                    </div>
+                    <div class="tfoot-popup-2 clearfix">
+                    </div>
+                </div>
+            </div>
+            <a title="Close" class="quickview-close close-window" href="javascript:;" onclick="$('#popup-cart').modal('hide');"><i class="fa  fa-close"></i></a>
+        </div>
+    
+    </div>
+    <div id="myModal" class="modal fade" role="dialog">
+    </div>
