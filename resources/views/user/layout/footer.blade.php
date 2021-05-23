7fed35074988b105d1be38da5194859f72b16dea
Quyet Ngo <nguynhuan123@gmail.com>
2021-05-23 22:12:49 +0700

    Update Zalo chat

--- resources/views/user/layout/footer.blade.php
+++ resources/views/user/layout/footer.blade.php
@@ -24,7 +24,8 @@
       </div>
 
 
-      <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+<!-- Zalo plugin chat -->
+      <div class="zalo-chat-widget" data-oaid="3162324188424086782" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
 
 <script src="https://sp.zalo.me/plugins/sdk.js"></script>
 
